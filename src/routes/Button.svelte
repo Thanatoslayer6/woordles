da2a271c93956c0153256b0b7e1fbd1e0ea3ea51
Thanatoslayer6 <Thanatoslayer6@gmail.com>
2023-05-11 13:32:46 +0800

    Saving progress for now

---
+++ src/routes/Button.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { goto } from "$app/navigation";
+    const handleClick = (route) => {
+        goto(route);
+    };
+</script>
+
+<button on:click={handleClick}>
+    <slot />
+</button>
+
+<style>
+    button {
+        padding: 1.5em;
+        border-radius: 0.25em;
+    }
+</style>
